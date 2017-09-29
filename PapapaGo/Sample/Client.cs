341e6823c4f928c7e60ad126d4d8690e96566a8b
Ethan Li <ethan.li@titansoft.com.sg>
2017-09-30 07:55:32 +0800

    add sample code

---
+++ PapapaGo/Sample/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+
+namespace PapapaGo.Sample
+{
+    public class Client
+    {
+        public void GetSearch()
+        {
+            var searchReqeust = new SearchRequest
+            {
+                StartStationCode = "ST_EZVVG1X5",
+                DestinationStationCode = "ST_D8NNN9ZK",
+                StartTime = DateTime.Now.AddDays(20),
+                NumberOfAdult = 1,
+                NumberOfChildren = 0
+            };
+
+            var dateTime = DateTime.Now.ToUniversalTime();
+            var secure = new ParamSecure(Config.Secret, Config.ApiKey, dateTime, searchReqeust);
+            var signature = secure.Sign();
+
+            var client = new RestClient(Config.GrailTravelHost);
+
+            var request = new RestRequest($"/api/v2/online_solutions?{searchReqeust.GetURL()}", Method.GET);
+            request.AddHeader("From", Config.ApiKey);
+            request.AddHeader("Date", dateTime.ToString("r"));
+            request.AddHeader("Authorization", signature);
+
+            var response = client.Get(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
---
+++ PapapaGo/Sample/SearchRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace PapapaGo.Sample
+{
+    public class SearchRequest : RequestBase
+    {
+        [JsonProperty("d")]
+        public string DestinationStationCode { get; set; }
+
+        [JsonProperty("na")]
+        public int NumberOfAdult { get; set; }
+
+        [JsonProperty("nc")]
+        public int NumberOfChildren { get; set; }
+
+        [JsonProperty("s")]
+        public string StartStationCode { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        [JsonProperty("dt")]
+        public string StartTimeString
+        {
+            get
+            {
+                return this.StartTime.ToString("yyyy-MM-dd HH:mm");
+            }
+        }
+    }
+}
