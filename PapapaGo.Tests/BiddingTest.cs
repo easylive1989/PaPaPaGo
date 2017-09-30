14dea6ddee8ab7f0fa423547908529d61c0e68c7
Ethan Li <ethan.li@titansoft.com.sg>
2017-09-30 17:03:01 +0800

    modify test case

--- PapapaGo.Tests/BiddingTest.cs
+++ PapapaGo.Tests/BiddingTest.cs
@@ -8,23 +8,23 @@ namespace PapapaGo.Tests
     public class BiddingTest
     {
         [TestMethod]
-        public void Amount_100_Multiple_2_MakeMoney_Should_Be_20()
+        public void Amount_100_Multiple_2_MakeMoney_Should_Be_0_2()
         {
             var target = new Bidding();
             target.Amount = 100;
             target.Multiple = 2;
-            var expectedResult = 20m;
+            var expectedResult = 0.20m;
 
             Assert.AreEqual(expectedResult, target.MakeMoney);
         }
 
         [TestMethod]
-        public void Amount_100_Multiple_2_Should_Be_200()
+        public void Amount_100_Multiple_2_Should_Be_2()
         {
             var target = new Bidding();
             target.Amount = 100;
             target.Multiple = 2;
-            var expectedResult = 200;
+            var expectedResult = 2;
 
             Assert.AreEqual(expectedResult, target.DisplayAmount);
         }
