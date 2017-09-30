29a19d894d13eb26be80cec08e56772ee6dd4846
WRayz <ruizhouwu@gmail.com>
2017-09-30 13:55:43 +0800

    導頁

---
+++ PapapaGo/Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PapapaGo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // POST:
+        // 訂票
+        public ActionResult OrderTicket()
+        {
+            return View();
+        }
+    }
+}
