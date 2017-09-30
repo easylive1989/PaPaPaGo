ade64dc73f7b45a0fa435ee946e1e9a491b1eca3
jean.lin52 <jean.lin52@TI03BY32>
2017-09-30 10:27:56 +0800

    change namespace

--- PapapaGo/Services/SearchService.cs
+++ PapapaGo/Services/SearchService.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
-using PapapaGo.Models;
+using PapapaGo.Models.Search;
 using PapapaGo.Sample;
 
 namespace PapapaGo.Services
