8c507326c9f8ea349a15bd8c6d3e5a152c6e1126
Ethan Li <ethan.li@titansoft.com.sg>
2017-09-30 09:47:10 +0800

    add confirm function

---
+++ PapapaGo/Sample/ConfirmRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+using Newtonsoft.Json;
+
+namespace PapapaGo.Sample
+{
+    public class ConfirmRequest : RequestBase
+    {
+        [JsonProperty("online_order_id")]
+        public string online_order_id { get; set; }
+    }
+}
