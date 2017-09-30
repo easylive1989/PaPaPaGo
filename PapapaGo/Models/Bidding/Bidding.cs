99306e2748d9adf5a50b1040054be57ca03fc36a
Scotwallace.Chen83 <Scotwallace.Chen@titansoft.com.sg>
2017-09-30 13:15:42 +0800

    Added Model.

---
+++ PapapaGo/Models/Bidding/Bidding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PapapaGo.Models.Bidding
+{
+    public class Bidding
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Amount { get; set; }
+        public int Multiple { get; set; }
+        public string BookingCode { get; set; }
+        public string Link { get; set; }
+        public string TicketJson { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
