911d50d27ca977b016637503510ce811328f8948
Scotwallace.Chen83 <Scotwallace.Chen@titansoft.com.sg>
2017-09-30 15:14:07 +0800

    cleaned code.

--- PapapaGo/Controllers/CustomerController.cs
+++ PapapaGo/Controllers/CustomerController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using PapapaGo.Models.DTO;
 using PapapaGo.Services;
@@ -10,8 +7,6 @@ namespace PapapaGo.Controllers
 {
     public class CustomerController : Controller
     {
-        // POST:
-        // 訂票
         public ActionResult OrderTicket()
         {
             return View();
