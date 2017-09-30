918728aafb2a41ea5d70b39ce525fbe07049e24c
jean.lin52 <jean.lin52@TI03BY32>
2017-09-30 10:27:10 +0800

    added search service

---
+++ PapapaGo/Services/SearchService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using PapapaGo.Models;
+using PapapaGo.Sample;
+
+namespace PapapaGo.Services
+{
+    public class SearchService
+    {
+        public string GetBookingKey()
+        {
+            var client = new Client();
+            var key = client.GetSearch();
+            var result = client.GetAsyncResult(key);
+
+            var searchResult = JsonConvert.DeserializeObject<List<SearchResponse>>(result).FirstOrDefault();
+            return searchResult.solutions[0].sections[0].offers[0].services[0].booking_code;
+        }
+    }
+}
