c97c1338de4925a26f0298ef68dd56337131d9d5
Ethan Li <ethan.li@titansoft.com.sg>
2017-09-30 14:35:50 +0800

    add BiddingTest

---
+++ PapapaGo.Tests/BiddingTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PapapaGo.Models.Bidding;
+
+namespace PapapaGo.Tests
+{
+    [TestClass]
+    public class BiddingTest
+    {
+        [TestMethod]
+        public void Amount_100_Multiple_2_Should_Be_200()
+        {
+            var target = new Bidding();
+            target.Amount = 100;
+            target.Multiple = 2;
+            var expectedResult = 200;
+
+            Assert.AreEqual(expectedResult, target.DisplayAmount);
+        }
+    }
+}
