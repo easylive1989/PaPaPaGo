d9affaa83de401be671407c13e411754c92a9451
kyoangel <gd92x2@hotmail.com>
2017-09-30 10:51:13 +0800

    handle html action

--- PapapaGo/Controllers/OrderTicketController.cs
+++ PapapaGo/Controllers/OrderTicketController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using PapapaGo.Models.Book;
 using PapapaGo.Sample;
+using PapapaGo.Services;
 
 namespace PapapaGo.Controllers
 {
@@ -10,12 +11,24 @@ namespace PapapaGo.Controllers
         // GET: OrderTicket
         public ActionResult Index()
         {
-            var client = new Client();
-            var resp = client.PostBook();
-            resp = client.GetAsyncResult(resp);
-            var bookReqeust = JsonConvert.DeserializeObject<BookResponse>(resp.Replace("\n", string.Empty));
-            ViewBag.Price = bookReqeust.tickets[0].price.cents;
+            //var client = new Client();
+            //var resp = client.PostBook();
+            //resp = client.GetAsyncResult(resp);
+            //var bookReqeust = JsonConvert.DeserializeObject<BookResponse>(resp.Replace("\n", string.Empty));
+            //ViewBag.Price = bookReqeust.tickets[0].price.cents;
             return View();
+        }
+
+        public bool Book()
+        {
+            var service = new SearchService();
+            var key = service.GetBookingKey();
+            //var client = new Client();
+            //var resp = client.PostBook();
+            //resp = client.GetAsyncResult(resp);
+            //var bookReqeust = JsonConvert.DeserializeObject<BookResponse>(resp);
+            //ViewBag.Price = bookReqeust.tickets[0].price.cents;
+            return true;
         }
     }
 }
