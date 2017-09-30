83153b60a17cbc4f8aeaa8993a97c523308cfcbd
kyoangel <gd92x2@hotmail.com>
2017-09-30 09:57:13 +0800

    add book response, some controller logic

--- PapapaGo/Controllers/OrderTicketController.cs
+++ PapapaGo/Controllers/OrderTicketController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using PapapaGo.Models;
+using PapapaGo.Sample;
 
 namespace PapapaGo.Controllers
 {
@@ -11,6 +14,11 @@ namespace PapapaGo.Controllers
         // GET: OrderTicket
         public ActionResult Index()
         {
+            var client = new Client();
+            var resp = client.PostBook();
+            resp = client.GetAsyncResult(resp);
+            var bookReqeust = JsonConvert.DeserializeObject<BookResponse>(resp);
+            ViewBag.Price = bookReqeust.tickets[0].price.cents;
             return View();
         }
     }
---
+++ PapapaGo/Models/BookResponse.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PapapaGo.Models
+{
+    public class BookResponse
+    {
+        public string id { get; set; }
+        public Railway railway { get; set; }
+        public From from { get; set; }
+        public To to { get; set; }
+        public DateTime departure { get; set; }
+        public int created_at { get; set; }
+        public TicketPrice ticket_price { get; set; }
+        public PaymentPrice payment_price { get; set; }
+        public RtpPrice rtp_price { get; set; }
+        public ChargingPrice charging_price { get; set; }
+        public RebateAmount rebate_amount { get; set; }
+        public List<Passenger> passengers { get; set; }
+        public List<Ticket> tickets { get; set; }
+        public List<Record> records { get; set; }
+    }
+
+    public class Railway
+    {
+        public string code { get; set; }
+    }
+
+    public class From
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class To
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class TicketPrice
+    {
+        public string currency { get; set; }
+        public int cents { get; set; }
+    }
+
+    public class PaymentPrice
+    {
+        public string currency { get; set; }
+        public int cents { get; set; }
+    }
+
+    public class RtpPrice
+    {
+        public string currency { get; set; }
+        public int cents { get; set; }
+    }
+
+    public class ChargingPrice
+    {
+        public string currency { get; set; }
+        public int cents { get; set; }
+    }
+
+    public class RebateAmount
+    {
+        public string currency { get; set; }
+        public int cents { get; set; }
+    }
+
+    public class Passenger
+    {
+        public string id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string birthdate { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string gender { get; set; }
+    }
+
+    public class From2
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class To2
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Price
+    {
+        public string currency { get; set; }
+        public int cents { get; set; }
+    }
+
+    public class Ticket
+    {
+        public string id { get; set; }
+        public From2 from { get; set; }
+        public To2 to { get; set; }
+        public Price price { get; set; }
+    }
+
+    public class Amount
+    {
+        public string currency { get; set; }
+        public int cents { get; set; }
+    }
+
+    public class Record
+    {
+        public string id { get; set; }
+        public Amount amount { get; set; }
+        public string type { get; set; }
+        public string category { get; set; }
+        public string target { get; set; }
+    }
+}
