e5a7d75de38d0d99936980ffd3d89c204ef8a44d
Scotwallace.Chen83 <Scotwallace.Chen@titansoft.com.sg>
2017-09-30 13:23:20 +0800

    Added Repo for bidding.

---
+++ PapapaGo/Repositories/BiddingRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PapapaGo.Models.Bidding;
+
+namespace PapapaGo.Repositories
+{
+    public class BiddingRepository : BaseRepository, IBiddingRepository
+    {
+        protected BiddingRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task<List<Bidding>> GetBiddingsAsync(string name)
+        {
+            return (await GetListAsync<Bidding>("WHERE Name = @name", new {name})).ToList();
+        }
+
+        public async Task<int> CreateBiddingAsync(Bidding bidding)
+        {
+            return (await InsertAsync(bidding)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PapapaGo/Repositories/IBiddingRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PapapaGo.Models.Bidding;
+
+namespace PapapaGo.Repositories
+{
+    public interface IBiddingRepository
+    {
+        Task<List<Bidding>> GetBiddingsAsync(string name);
+
+        Task<int> CreateBiddingAsync(Bidding bidding);
+    }
+}
