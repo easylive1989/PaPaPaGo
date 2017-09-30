15e232662c79ab3570383fc8bd9687bc41fd6a24
Scotwallace.Chen83 <Scotwallace.Chen@titansoft.com.sg>
2017-09-30 09:37:01 +0800

    Added PassengerRepository.

---
+++ PapapaGo/Repositories/PassengerRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PapapaGo.Sample;
+
+namespace PapapaGo.Repositories
+{
+    public class PassengerRepository : BaseRepository, IPassengerRepository
+    {
+        protected PassengerRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task<List<Passenger>> GetAllPassengersAsync()
+        {
+            return (await GetListAsync<Passenger>("WHERE 1")).ToList();
+        }
+
+        public async Task<Passenger> GetPassengerAsync(string firstName, string lastName)
+        {
+            return (await GetListAsync<Passenger>("WHERE first_name = @firstName AND last_name = @lastName",
+                new {firstName, lastName})).FirstOrDefault();
+        }
+
+        public async Task<Passenger> GetPassengerAsync(string postCode)
+        {
+            return (await GetListAsync<Passenger>("WHERE first_name = @firstName AND last_name = @lastName",
+                new { postCode })).FirstOrDefault();
+        }
+
+        public async Task<int> CreatePassengerAsync(Passenger passenger)
+        {
+            return (await InsertAsync(passenger)).GetValueOrDefault();
+        }
+    }
+}
