48c550260c4c87393e8e34fc0b6e8bace72344d4
kyoangel <gd92x2@hotmail.com>
2017-09-30 14:37:30 +0800

    put data to modle to save to db

--- PapapaGo/Controllers/BidController.cs
+++ PapapaGo/Controllers/BidController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using PapapaGo.Services;
 
 namespace PapapaGo.Controllers
@@ -6,7 +7,7 @@ namespace PapapaGo.Controllers
     public class BidController : Controller
     {
         // GET: Bid
-        public ActionResult Index()
+        public ActionResult Index(string oriSelect, string desSelect, string username, string datetime, string fblink, string priceSelect)
         {
             var bidService = new BidService();
             var biddingTickets = bidService.GetBidInfo();
